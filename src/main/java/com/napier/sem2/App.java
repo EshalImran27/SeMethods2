0ff13c095a9191928b6c8639cfd1c8b5dadb6d01
EshalImran27 <40735351@live.napier.ac.uk>
2025-10-10 13:19:53 +0100

    updated app.java file such that it communicates via sqldatabase rather than mongodb server.And updated dependencies in pom.xml

--- src/main/java/com/napier/sem2/App.java
+++ src/main/java/com/napier/sem2/App.java
@@ -1,35 +1,72 @@
 package com.napier.sem2;
 
-import com.mongodb.MongoClient;
-import com.mongodb.client.MongoDatabase;
-import com.mongodb.client.MongoCollection;
-import org.bson.Document;
+import java.sql.*;
 
 public class App
 {
     public static void main(String[] args)
     {
-        // Connect to MongoDB on local system - we're using port 27000
-        // Connect to MongoDB
-        try(MongoClient mongoClient = new MongoClient("mongo-dbserver")) {
-            // Get a database - will create when we use it
-            MongoDatabase database = mongoClient.getDatabase("mydb");
-            // Get a collection from the database
-            MongoCollection<Document> collection = database.getCollection("test");
-            // Create a document to store
-            Document doc = new Document("name", "SeMethods2")
-                    .append("class", "DevOps")
-                    .append("year", "2025")
-                    .append("result", new Document("CW", 95).append("EX", 85));
-            // Add document to collection
-            collection.insertOne(doc);
-
-            // Check document in collection
-            Document myDoc = collection.find().first();
-            System.out.println(myDoc.toJson());
+        try
+        {
+            // Load MySQL Database driver
+            Class.forName("com.mysql.cj.jdbc.Driver");
         }
-        catch(Exception ex){
-            System.err.println("An error occurred: " + ex.getMessage());
+        catch (ClassNotFoundException e)
+        {
+            System.out.println("Could not load SQL driver");
+            System.exit(-1);
+        }
+
+        // Connection to the database
+        Connection con = null;
+        int retries = 100;
+        for (int i = 0; i < retries; ++i)
+        {
+            System.out.println("Connecting to MySQL database...");
+            try
+            {
+                // Wait a bit for db to start
+                Thread.sleep(30000);
+                // Connect to MySQL WORLD database
+                con = DriverManager.getConnection("jdbc:mysql://db:3306/world?allowPublicKeyRetrieval=true&useSSL=false", "root", "example");
+                System.out.println("Successfully connected to World database!");
+
+                // Test the connection by querying the database
+                Statement stmt = con.createStatement();
+                ResultSet rset = stmt.executeQuery("SHOW TABLES");
+                System.out.println("Tables in World database:");
+                while (rset.next()) {
+                    System.out.println(" - " + rset.getString(1));
+                }
+
+                // Wait a bit
+                Thread.sleep(10000);
+                // Exit for loop
+                break;
+            }
+            catch (SQLException sqle)
+            {
+                System.out.println("Failed to connect to database attempt " + Integer.toString(i));
+                System.out.println(sqle.getMessage());
+            }
+            catch (InterruptedException ie)
+            {
+                System.out.println("Thread interrupted? Should not happen.");
+            }
+        }
+
+        if (con != null)
+        {
+            try
+            {
+                // Close connection
+                con.close();
+                System.out.println("Database connection closed.");
+            }
+            catch (Exception e)
+            {
+                System.out.println("Error closing connection to database");
+            }
         }
     }
 }
