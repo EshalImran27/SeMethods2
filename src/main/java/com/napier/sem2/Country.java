431d0d5a3066f8639cf4be828b69204c8fa68315
EshalImran27 <40735351@live.napier.ac.uk>
2025-10-17 10:51:04 +0100

    adding comments

--- src/main/java/com/napier/sem2/Country.java
+++ src/main/java/com/napier/sem2/Country.java
@@ -1,11 +1,12 @@
+//including package in the class
 package com.napier.sem2;
-
+//adding attributes into the country class
 public class Country {
-    public String code;
-    public String name;
-    public String continent;
-    public String region;
-    public String capital;
-    public int population;
+    public String code; //separate code for each country
+    public String name; //specific name of a country
+    public String continent; //continent in which the country is present
+    public String region; //region of the country
+    public String capital; //capital city of the specific country
+    public int population; //total population of that country
 
 }
