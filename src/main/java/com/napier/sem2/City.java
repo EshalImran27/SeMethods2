8deefdf7907e2ea6ecf87c7f0a49475ce37791f3
Pablo Ramallo <arucaspablo@hotmail.com>
2025-11-08 23:25:16 +0000

    Add City class, Add CapitalTests and Rename CountryQueries into Query to hold all Queries of the app

---
+++ src/main/java/com/napier/sem2/City.java
@@ -0,0 +1,73 @@
+package com.napier.sem2;
+
+import java.sql.*;
+import java.util.List;
+
+public class City {
+    //Fields
+    private String name = "";
+    private String country = "";
+    private String district = "";
+    private int population = 0;
+
+    //Constructors
+    //All default values
+    public City() {}
+    //Assign all values
+    public City(String name, String country, String district, int population)
+    {
+        this.name = name;
+        this.country = country;
+        this.district = district;
+        this.population = population;
+    }
+    //Capital Constructor
+    public City(String name, String country, int population)
+    {
+        this.name = name;
+        this.country = country;
+        this.population = population;
+    }
+
+    //Set & Get Methods
+    public String getName() {return name;}
+    public void setName(String name) {this.name = name;}
+    public String getCountry() {return country;}
+    public void setCountry(String country) {this.country = country;}
+    public String getDistrict() {return district;}
+    public void setDistrict(String district) {this.district = district;}
+    public int getPopulation() {return population;}
+    public void setPopulation(int population) {this.population = population;}
+
+    //Display Method
+    public  void displayCapital(){
+        System.out.println(
+                String.format(
+                        "%-30s %-15s %10d",
+                        this.name != null ? this.name : "N/A",
+                        this.country != null ? this.country : "N/A",
+                        this.population
+                )
+        );
+    }
+
+    //Method for display a list of Capitals
+    public static void displayListOfCapital(List<City> listOfCapital)
+    {
+        if (listOfCapital == null || listOfCapital.isEmpty()) {
+            System.out.println("No capitals can be displayed");
+            return;
+        }
+        System.out.println("=================================================================");
+        System.out.println(String.format("%-30s %-15s %10s",
+                "Name", "Country", "Population"));
+        System.out.println("-----------------------------------------------------------------");
+        for (City city : listOfCapital) {
+            if (city == null) {
+                System.out.println("Warning: capital is null");
+                continue;
+            }
+            city.displayCapital();
+        }
+    }
+}
--- src/main/java/com/napier/sem2/Query.java
+++ src/main/java/com/napier/sem2/Query.java
@@ -4,9 +4,9 @@ import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
-public class CountryQueries {
+public class Query {
     private Connection con;
-    public CountryQueries(Connection con) {
+    public Query(Connection con) {
         this.con = con;
     }
     private List<Country> CountriesFromResultSet(ResultSet rset) throws SQLException {
@@ -91,10 +91,64 @@ public class CountryQueries {
         System.out.println("Top " + n + " Countries in the region " + region + " ranked from largest population to smallest: ");
         SqlQuery(sqlStatement);
     }
+    //Capital Queries
+    public ArrayList<City> reportCapitalGlobal(Connection database)
+    {
+        ArrayList<City> report = new ArrayList<City>();
+        //database connection check
+        if (database == null){
+            System.out.println("Database Connection is null");
+            return report;
+        }
+        try{
+            Statement stmt= database.createStatement();
+            String sqlStatement = "SELECT city.Name, country.Name, city.Population FROM city "
+                    + "JOIN country ON city.CountryCode = country.Code "
+                    + "ORDER BY city.Population DESC";
+            ResultSet rset=stmt.executeQuery(sqlStatement);//execution of sql query
+            while(rset.next()){
+                City aux = new City();
+                aux.setName(rset.getString("city.Name"));
+                aux.setCountry(rset.getString("country.Name"));
+                aux.setPopulation(rset.getInt("city.Population"));
+                report.add(aux);
+            }
+            rset.close();
+            stmt.close();
+        } catch(SQLException e){
+            System.out.println("Error getting capitals by population in world" +
+                    e.getMessage());
+            System.out.println("failed to get capitals details");
+        }
+        return report;
+    }
 
+    public ArrayList<City> reportCapitalContinent(String continent)
+    {
+        ArrayList<City> report = new ArrayList<City>();
+        return report;
+    }
 
+    public ArrayList<City> reportCapitalRegion(String region)
+    {
+        ArrayList<City> report = new ArrayList<City>();
+        return report;
+    }
+    public ArrayList<City> topReportCapitalGlobal(int top)
+    {
+        ArrayList<City> report = new ArrayList<City>();
+        return report;
+    }
 
+    public ArrayList<City> topReportCapitalContinent(int top, String continent)
+    {
+        ArrayList<City> report = new ArrayList<City>();
+        return report;
+    }
 
-
-
+    public ArrayList<City> topReportCapitalRegion(int top, String region)
+    {
+        ArrayList<City> report = new ArrayList<City>();
+        return report;
+    }
 }
