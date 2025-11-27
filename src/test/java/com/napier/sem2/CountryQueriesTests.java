f5a8d3a58a1483700f5eef6c9cabeb0c35161fe8
EshalImran27 <40735351@live.napier.ac.uk>
2025-11-27 17:07:36 +0000

    fixed errors caused my PMD violations

--- src/test/java/com/napier/sem2/CountryQueriesTests.java
+++ src/test/java/com/napier/sem2/CountryQueriesTests.java
@@ -16,7 +16,7 @@ public class CountryQueriesTests {
     private ResultSet MockResultSet;
     private CountryQueries MockCountryQueries;
     private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
-    private Country mockCountry;
+    private List<Country> mockCountry;
     private Handler testHandler;
     private Logger countryQueriesLogger;
     private Logger countryLogger;
